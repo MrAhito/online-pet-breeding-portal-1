64e0b176064ea725b086aa4c4b77bf780f043697
MrAhito <rolandoallanofficial@gmail.com>
2021-05-07 07:11:05 +0800

    zc

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import HomePage from './pages/HomePage';
 import Dashboard from './pages/Dashboard';
+import Admin_Dash from './components/Admin_Dash';
+import Adm_post from './pages/Adm_post';
+import Adm_Message from './pages/Adm_Message';
+import Adm_Team from './pages/Adm_Team';
+import Adm_Report from './pages/Adm_Report';
 
 function App() {
   return (
@@ -12,10 +17,23 @@ function App() {
           <Route path='/' exact component={HomePage} />
         </Switch>
         <Switch>
-        </Switch>
-        <Switch>
          <Route path='/dashboard' exact component={Dashboard} />
         </Switch>
+        <Switch>
+        <Route path='/dashboard' exact component={Admin_Dash} />
+    </Switch>
+    <Switch>
+        <Route path='/post' exact component={Adm_post} />
+    </Switch>
+    <Switch>
+        <Route path='/messages' exact component={Adm_Message} />
+    </Switch>
+    <Switch>
+        <Route path='/team' exact component={Adm_Team} />
+    </Switch>
+    <Switch>
+        <Route path='/reports' exact component={Adm_Report} />
+    </Switch>
       </Router>
     </>
   );
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,36 +1,11 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Adm_Message from './Adm_Message';
-import Adm_post from './Adm_post';
-import Admin_Dash from '../components/Admin_Dash';
-import Adm_Report from './Adm_Report';
-import Adm_Team from './Adm_Team';
-import HomePage from './HomePage';
+import adminDash from '../components/Admin_Dash';
 
 class Dashboard extends Component {
     render() {
         return (
             <>
-                <Router>
-                    <Switch>
-                        <Route path='/dashboard' exact component={Admin_Dash} />
-                    </Switch>
-                    <Switch>
-                        <Route path='/' exact component={HomePage} />
-                    </Switch>
-                    <Switch>
-                        <Route path='/post' exact component={Adm_post} />
-                    </Switch>
-                    <Switch>
-                        <Route path='/messages' exact component={Adm_Message} />
-                    </Switch>
-                    <Switch>
-                        <Route path='/team' exact component={Adm_Team} />
-                    </Switch>
-                    <Switch>
-                        <Route path='/reports' exact component={Adm_Report} />
-                    </Switch>
-                </Router>
+                <adminDash/>
             </>
         )
     }
