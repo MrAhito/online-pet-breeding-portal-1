cd45f4f0d8b41d02ea5a3de14de6f2e9fbfd9a35
MrAhito <rolandoallanofficial@gmail.com>
2021-05-07 15:40:38 +0800

    S

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import SideBar from '../components/SideBar'
 class Dashboard extends Component {
     render() {
         return (
     <>
-       
+    <SideBar/>
+       <Router>
+           <Switch>
+                <Route path='/dashboard' exact component={AdminDash} />
+            </Switch>
+            <Switch>
+                <Route path='/post' exact component={AdmPost} />
+            </Switch>
+            <Switch>
+                <Route path='/messages' exact component={AdmMessage} />
+            </Switch>
+            <Switch>
+                <Route path='/team' exact component={AdmTeam} />
+            </Switch>
+            <Switch>
+                <Route path='/reports' exact component={AdmReport} />
+            </Switch>
+       </Router>
     </>
         )
     }
