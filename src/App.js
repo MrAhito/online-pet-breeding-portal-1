ed7c868a4e3ee7cba128bc66cfe242f9ccea96ff
MrAhito <rolandoallanofficial@gmail.com>
2021-05-09 06:17:45 +0800

    da

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
 import HomePage from './pages/HomePage';
 import Dashboard from './pages/Dashboard';
+import Admin from './pages/Admin';
 function App() {
 
   return (
@@ -14,6 +15,9 @@ function App() {
         <Switch>
           <Route path='/dashboard' exact component={Dashboard} />
         </Switch>
+        <Switch>
+        <Route path='/admin' exact component={Admin} />
+      </Switch>
       </Router>
     </>
   );
---
+++ src/pages/Admin.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import AdminNav from '../components/AdminNav'
+
+ class Admin extends Component {
+    render() {
+        return (
+            <>
+                <AdminNav/>
+            </>
+        )
+    }
+}
+
+export default Admin
