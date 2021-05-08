b4fb5b298a1a584fd9b1f4459d28f7079b5e81e5
MrAhito <rolandoallanofficial@gmail.com>
2021-05-09 06:49:32 +0800

    wdfd

--- src/components/AdminNav.js
+++ src/components/AdminNav.js
@@ -1,17 +1,45 @@
-import React from 'react'
+import React, {useState} from 'react'
 import LogoNav from '../images/logo.png'
 import * as aiIcons from 'react-icons/cg'
+import Dp from '../images/icon.png'
 import './AdminNav.css'
-
+import './SideBar.css'
+import { Link } from 'react-router-dom'
+import { SideBarData } from './SideBarData'
 function AdminNav() {
+    
+    const [sidebar, setSidebar] = useState(false);
+    const showSideBar = () => setSidebar(!sidebar);
     return (
         <>
            <div className='nav adminNav'>
-                <aiIcons.CgMenuGridR className='userNavIcons' />
+                <aiIcons.CgMenuGridR className='userNavIcons' onClick={showSideBar} />
                 <img alt="logo" src={LogoNav} className="nav-logo" />
+                <img alt='Profile' src={Dp} className='iconAdmin'/>
                 <aiIcons.CgLogOut className='userNavIcons' title='LogOut' />
             </div>
-            
+            <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
+
+                    <ul className="nav-menu-items">
+                        <li className="navbar-toogle">
+                            <Link to='#' className='side-menuBars' >
+                                <aiIcons.CgClose onClick={showSideBar} />
+                            </Link>
+                        </li>
+                        <br />
+                        {SideBarData.map((item, index) => {
+                            return (
+                                <li key={index} className={item.cName}>
+                                    <Link onClick={showSideBar} to={item.path}>
+                                        {item.icon}
+                                        <span>{item.title}</span>
+                                    </Link>
+                                </li>
+                            )
+                        })}
+                    </ul>
+                </nav>
+
         </>
     )
 }
--- src/components/NotifDiv.js
+++ src/components/NotifDiv.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './NotifDiv.css'
 
 function NotifDiv(props) {
+     
     return (
         <div className='notify'>
            <div className='title_div'>{props.titleName}</div>
