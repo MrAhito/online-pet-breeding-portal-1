4596c2d1c12ef1faf21a61bad8da4ae375e71cd1
MrAhito <rolandoallanofficial@gmail.com>
2021-05-10 14:43:26 +0800

    aefaead

--- src/components/USerPostDiv.js
+++ src/components/USerPostDiv.js
@@ -1,39 +1,28 @@
-import React, { useState } from 'react'
+import React, { Component } from 'react'
 import Ivon from '../images/icon.png'
-import * as aiIcons from 'react-icons/fi'
 import './USerPostDiv.css'
 import PostedData from './Posted_Data'
 
-function USerPostDiv() {
-  const [addPostDiv, setaddPostDiv] = useState(false);
-  const showPosting = () => setaddPostDiv(!addPostDiv);
-
-  return (
-
+ class USerPostDiv extends Component {
+  
+    render() {
+        return (
             <>
               <div className='New_post'>
               <div className='user_post'>
-                <img src={Ivon} alt='icon' className='iconUserPost' />
-          <input type='text' name='textPost' onClick={showPosting} className='inputUserPost' placeholder='Create a Post  here....'></input>
+              <img src={Ivon} alt='icon' className='iconUserPost'/>
+              <input type='text' name='textPost' className='inputUserPost' placeholder='Create a Post  here....'></input>
               </div>
-
               <div className='btton_post'>
-                <div className='ImageAdd' title='Add Image'><aiIcons.FiImage className='iconimg' /><span>Add Image</span></div>
-                <div className='TagAdd' title='Add Tag'><aiIcons.FiTag className='icontag' /><span>Add Tag</span></div>
-              </div>
-            </div>
-
-      <div className={addPostDiv ? 'posting_div shaw' : 'posting_div'} >
-        <div className='blocker' onClick={showPosting} />
-              <div className='AddPostCon'>
-        </div>
-
+              <input type='file'/>
+                </div>
               </div>
             <div className='PostedDataa'>
             <PostedData />
             </div>
             </>
         )
+    }
 }
 
 export default USerPostDiv
