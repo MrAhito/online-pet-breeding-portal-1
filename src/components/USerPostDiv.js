90f548cd1a4335246a0bdeb83649cd94bbf7ce9e
MrAhito <rolandoallanofficial@gmail.com>
2021-05-10 13:49:57 +0800

    asdad

--- src/components/USerPostDiv.js
+++ src/components/USerPostDiv.js
@@ -1,21 +1,41 @@
 import React, { Component } from 'react'
 import Ivon from '../images/icon.png'
+import * as aiIcons from 'react-icons/fi'
 import './USerPostDiv.css'
 import PostedData from './Posted_Data'
 
  class USerPostDiv extends Component {
-  
+   constructor() {
+     super()
+     this.state = {
+       addPostDi: false
+     }
+   }
+   viewPosting(valua) {
+     this.setState({
+       addPostDiv: valua
+     })
+   }
     render() {
         return (
             <>
               <div className='New_post'>
               <div className='user_post'>
-              <img src={Ivon} alt='icon' className='iconUserPost'/>
-              <input type='text' name='textPost' className='inputUserPost' placeholder='Create a Post  here....'></input>
+                <img src={Ivon} alt='icon' className='iconUserPost' />
+                <input type='text' name='textPost' className='inputUserPost' placeholder='Create a Post  here....'></input>
               </div>
+
               <div className='btton_post'>
-              <input type='file'/>
-                </div>
+                <div className='ImageAdd' title='Add Image'><aiIcons.FiImage className='iconimg' /><span>Add Image</span></div>
+                <div className='TagAdd' title='Add Tag'><aiIcons.FiTag className='icontag' /><span>Add Tag</span></div>
+              </div>
+            </div>
+
+            <div className={this.state.addPostDiv ? 'posting_div shaw' : 'posting_div'} >
+              <div className='AddPostCon'>
+              </div>
+              <div className='blocker' onClick={(e) => this.viewPosting(false)} />
+
               </div>
             <div className='PostedDataa'>
             <PostedData />
