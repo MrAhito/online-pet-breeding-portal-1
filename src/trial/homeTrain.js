b06b02d430790964182c96d5b0a8c011292a69dc
MrAhito <rolandoallanofficial@gmail.com>
2021-05-10 11:51:53 +0800

    awdf

--- src/trial/homeTrain.js
+++ src/trial/homeTrain.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react'
 import './homeTrain.css'
-import TrialPage from './TrialPage'
 
  class homeTrain extends Component {
     render() {
         return (
             <>
             <div className='train_div'>
-            <h1> Hello I am the Train page Header</h1>
-            <TrialPage/>
+                    <h1> Hello I am the Train page Header</h1>
             </div>
             </>
         )
