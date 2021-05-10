9a2a996c108dc6090b703546a35f16d79960034b
MrAhito <rolandoallanofficial@gmail.com>
2021-05-10 15:28:00 +0800

    wafds

--- src/components/USerPostDiv.js
+++ src/components/USerPostDiv.js
@@ -3,8 +3,7 @@ import Ivon from '../images/icon.png'
 import './USerPostDiv.css'
 import PostedData from './Posted_Data'
 
- class USerPostDiv extends Component {
-  
+class USerPostDiv extends Component {
    constructor() {
      super()
      this.state = {
@@ -25,10 +24,19 @@ import PostedData from './Posted_Data'
               <img src={Ivon} alt='icon' className='iconUserPost'/>
               <input type='text' name='textPost' className='inputUserPost' placeholder='Create a Post  here....'></input>
               </div>
+
               <div className='btton_post'>
-              <input type='file'/>
-                </div>
+                <div className='ImageAdd' title='Add Image'><aiIcons.FiImage className='iconimg' /><span>Add Image</span></div>
+                <div className='TagAdd' title='Add Tag'><aiIcons.FiTag className='icontag' /><span>Add Tag</span></div>
               </div>
+              </div>
+
+            <div className={this.state.addPostDiv ? 'posting_div shaw' : 'posting_div'} >
+              <div className='AddPostCon'>
+              </div>
+              <div className='blocker' onClick={(e) => this.viewPosting(false)} />
+            </div>
+
             <div className='PostedDataa'>
             <PostedData />
             </div>
